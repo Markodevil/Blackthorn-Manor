0fe14527ef42095fdf846126849d2611d0afd0f4
Gabriel4224 <31612343+Gabriel4224@users.noreply.github.com>
2018-07-17 13:55:08 +1000

    Added FPSCamera and PlayerMovement scripts

---
+++ The House/Assets/Scripts/Player/FPSCamera.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSCamera : MonoBehaviour {
+
+    public Transform PlayerBody; 
+    public float Sensitivity;
+
+    float XAxisClamp = 0; 
+    // Use this for initialization
+    
+
+    // Update is called once per frame
+    void Update () {
+        Cursor.lockState = CursorLockMode.Locked;
+
+        RotateCamera();
+    }
+    void RotateCamera()
+    {
+        float MouseX = Input.GetAxis("Mouse X");
+        float MouseY = Input.GetAxis("Mouse Y");
+
+        float RotAmountX = MouseX * Sensitivity;
+
+        float RotAmountY = MouseY * Sensitivity;
+
+        Vector3 TargetRotationCamera = transform.rotation.eulerAngles;
+        Vector3 TargetRotationBody = PlayerBody.rotation.eulerAngles;
+
+        XAxisClamp -= RotAmountY;
+
+        TargetRotationCamera.x -= RotAmountY;
+        TargetRotationCamera.z = 0;
+
+        TargetRotationBody.y += RotAmountX;
+
+        if (XAxisClamp > 90)
+        {
+            XAxisClamp = 90;
+            TargetRotationCamera.x = 90;
+        }
+        else if (XAxisClamp < -90)
+        {
+            XAxisClamp = -90;
+            TargetRotationCamera.x = 270;
+        }
+
+
+        transform.rotation = Quaternion.Euler(TargetRotationCamera);
+        PlayerBody.rotation = Quaternion.Euler(TargetRotationBody);
+    }
+
+}
---
+++ The House/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+
+    CharacterController charControl; 
+    public float Speed;
+    bool LookingAtCameras = false; 
+	// Use this for initialization
+	void Awake ()
+    {
+        charControl = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        Movement();
+
+        
+    }
+
+    void Movement()
+    {
+
+        float Horizontal = Input.GetAxis("Horizontal");
+        float Vertcile = Input.GetAxis("Vertical");
+
+        Vector3 MoveDirectionSide = transform.right * Horizontal * Speed;
+        Vector3 MoveDirectionForward = transform.forward * Vertcile * Speed;
+
+        charControl.SimpleMove(MoveDirectionSide);
+        charControl.SimpleMove(MoveDirectionForward);
+    }
+}
