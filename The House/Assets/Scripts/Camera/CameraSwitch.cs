fa15f75aa6cee6ed31c162d68310e6ca78186909
Deeon Burgan <dburgan@live.com>
2018-07-18 09:49:19 +1000

    Added Deeon's scene and Camera related scripts

---
+++ The House/Assets/Scripts/Camera/CameraSwitch.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+
+    public RenderTexture renderTexture;
+    public int cameraIndex = 0;
+    public GameObject[] cameras;
+    public GameObject phoneThing;
+    public bool lookingAtPhone = false;
+    public MonoBehaviour[] playerScriptsToBeToggled;
+    public GameObject playerCamera;
+
+    // Use this for initialization
+    void Start()
+    {
+        foreach (GameObject cam in cameras)
+        {
+            cam.SetActive(false);
+        }
+        cameras[0].SetActive(true);
+        playerCamera = GetComponentInChildren<Camera>().gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!lookingAtPhone)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                for (int i = 0; i < playerScriptsToBeToggled.Length; i++)
+                {
+                    playerScriptsToBeToggled[i].enabled = false;
+                }
+                phoneThing.SetActive(true);
+                lookingAtPhone = true;
+            }
+
+        }
+
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                cameras[cameraIndex].SetActive(false);
+                if (cameraIndex == 0)
+                    cameraIndex = cameras.Length - 1;
+                else
+                    cameraIndex--;
+                cameras[cameraIndex].SetActive(true);
+            }
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                cameras[cameraIndex].SetActive(false);
+                if (cameraIndex == cameras.Length - 1)
+                    cameraIndex = 0;
+                else
+                    cameraIndex++;
+                cameras[cameraIndex].SetActive(true);
+            }
+
+            if(Input.GetKeyDown(KeyCode.F))
+            {
+                for (int i = 0; i < playerScriptsToBeToggled.Length; i++)
+                {
+                    playerScriptsToBeToggled[i].enabled = true;
+                }
+                phoneThing.SetActive(false);
+                lookingAtPhone = false;
+            }
+        }
+    }
+}
---
+++ The House/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public string codeInit;
+    public int codeIndex = 0;
+    private string code1;
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.anyKeyDown)
+        {
+            if(Input.GetKeyDown(codeInit[codeIndex].ToString()))
+            {
+                codeIndex++;
+            }
+            else
+            {
+                codeIndex = 0;
+            }
+        }
+
+        if (codeIndex == codeInit.Length)
+        {
+            Debug.Log("code put in");
+        }
+	}
+}
