522d113d2c0d98c5ab5858ec45941d0622bf20fe
Fryzeec <32441156+Fryzeec@users.noreply.github.com>
2018-07-18 12:42:05 +1000

    Moved AI Scripts

--- The House/Assets/Scripts/AI/Ghost.cs
+++ The House/Assets/Scripts/AI/Ghost.cs
@@ -4,41 +4,47 @@ using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 using SIGHT;
-public class Ghost : MonoBehaviour {
+public class Ghost : MonoBehaviour
+{
 
+    //Sets the player/patrol point as the desired destination
     [SerializeField]
     public Transform destination;
 
     NavMeshAgent navMeshAgent;
 
+    //access to the sight.cs
     Sight enemySight;
 
-	// Use this for initialization
-	void Start () {
+    
+    void Start()
+    {
         navMeshAgent = this.GetComponent<NavMeshAgent>();
         enemySight = this.GetComponent<Sight>();
 
+        //Nav mesh failed
         if (navMeshAgent == null)
         {
-            //Nav mesh failed
             Debug.LogError("The Nav Mesh agent component is not attached to " + gameObject.name);
             return;
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
 
+    
+    void Update()
+    {
+        //Checks if the Player is within the Ghosts vision
         if (enemySight.visibleTargets.Capacity > 0)
             SetDestination();
         else
         {
-            //partrol
+            //partrol / Search
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        //Game Over on player collision
         if (collision.gameObject.layer == 8)
         {
             destination = null;
