9ff0c16089dd5afe6b55a6f5939f5ba6e649d024
Fryzeec <32441156+Fryzeec@users.noreply.github.com>
2018-07-18 11:41:37 +1000

    Basic Ghost.cs, Yet to add patrol and wander.

---
+++ The House/Assets/Scripts/Ghost.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+using SIGHT;
+public class Ghost : MonoBehaviour {
+
+    [SerializeField]
+    public Transform destination;
+
+    NavMeshAgent navMeshAgent;
+
+    Sight enemySight;
+
+	// Use this for initialization
+	void Start () {
+        navMeshAgent = this.GetComponent<NavMeshAgent>();
+        enemySight = this.GetComponent<Sight>();
+
+        if (navMeshAgent == null)
+        {
+            //Nav mesh failed
+            Debug.LogError("The Nav Mesh agent component is not attached to " + gameObject.name);
+            return;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (enemySight.visibleTargets.Capacity > 0)
+            SetDestination();
+        else
+        {
+            //partrol
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            destination = null;
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+
+    private void SetDestination()
+    {
+        if (destination != null)
+        {
+            Vector3 targetVector = destination.transform.position;
+            navMeshAgent.SetDestination(targetVector);
+        }
+    }
+
+}
---
+++ The House/Assets/Scripts/Sight Editor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+//This script was written as a youtube tutorial by  Sebastian Lague https://www.youtube.com/watch?v=rQG9aUWarwE
+[CustomEditor(typeof (SIGHT.Sight))]
+public class SightEditor : Editor {
+
+    void OnSceneGUI()
+    {
+        SIGHT.Sight fov = (SIGHT.Sight)target;
+        Handles.color = Color.black;
+        Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360, fov.viewRadius);
+        Vector3 viewAngleA = fov.DirFromAngle(-fov.viewAngle / 2, false);
+        Vector3 viewAngleB = fov.DirFromAngle(fov.viewAngle / 2, false);
+
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleA * fov.viewRadius);
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleB * fov.viewRadius);
+
+        Handles.color = Color.red;
+        foreach (Transform visibleTarget in fov.visibleTargets)
+        {
+            Handles.DrawLine(fov.transform.position, visibleTarget.position);
+        }
+    }
+}
