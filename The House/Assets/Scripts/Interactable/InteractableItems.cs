5156ec15c31e825d3a8ecd924ba71dd8822e1c70
Deeon Burgan <dburgan@live.com>
2018-07-25 12:04:45 +1000

    All interactables now require an audio source

--- The House/Assets/Scripts/Interactable/InteractableItems.cs
+++ The House/Assets/Scripts/Interactable/InteractableItems.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(AudioSource))]
 public class InteractableItems : MonoBehaviour
 {
 
