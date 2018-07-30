ff7fcd77f70e5c2b7d02e5066fd6a8d62a78fbb8
Gabriel4224 <31612343+Gabriel4224@users.noreply.github.com>
2018-07-30 14:03:32 +1000

    Fixed the door

--- The House/Assets/Scripts/Interactable/HingeDoorScript.cs
+++ The House/Assets/Scripts/Interactable/HingeDoorScript.cs
@@ -31,9 +31,9 @@ public class HingeDoorScript : MonoBehaviour {
         Vector3 HingePosition = transform.position;
         Vector3 Playerposition = Player.transform.position;
 
-        Vector3 Direction = HingePosition = Playerposition;
+        Vector3 Direction = HingePosition - Playerposition;
 
-        Vector3.Normalize(Direction);
+        Direction.Normalize();
 
         //--------------------------------------------------------------------------------------
         // Checks if the players posiition is infront or behind the door 
@@ -45,50 +45,14 @@ public class HingeDoorScript : MonoBehaviour {
         //      doors on either side 
         //--------------------------------------------------------------------------------------
 
-
-        //MAKE SURE DOORS X AXIS IS FACING THE PLAYER WHEN FIRST OPENING THE DOOR 
-        if (Vector3.Dot(Direction, Player.transform.right) > 0)
+   
+        if (isOpen)
         {
-         //   rb.AddTorque(transform.up * mouseY);
-
-            Debug.Log("ZERO");
-            if (isOpen && mouseY > 0)
-            {
-                //Quaternion targetRotation = Quaternion.Euler(0, doorCloseAngle, 0);
-                //transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, mouseY * Time.deltaTime * doorOpenSpeed);
-                rb.AddForce(-transform.forward * mouseY * doorOpenSpeed);
-                Debug.Log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAND OPEN");
-
-            }
-
-
-            if (isOpen && mouseY < 0)
-            {
-                // Quaternion targetRotation2 = Quaternion.Euler(0, doorOpenAngle, 0);
-                // transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation2, -mouseY * Time.deltaTime * doorOpenSpeed);
-                rb.AddForce(transform.forward * -mouseY * doorOpenSpeed);
-
-            }
-
+   
+                rb.AddForceAtPosition(Player.transform.forward * mouseY * doorOpenSpeed, Player.transform.position);
 
         }
-        else
-        {
-            Debug.Log("Greater Than Zero");
-            if (isOpen && mouseY > 0)
-            {
-                rb.AddForce(transform.forward * mouseY * doorOpenSpeed);
-
-            }
-
-
-            if (isOpen && mouseY < 0)
-            {
-                rb.AddForce(transform.forward * mouseY * doorOpenSpeed);
-                Debug.Log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAND CLOSED");
-
-            }
-        }
+       
     }
 
     public void changeDoorState()
