5ae26b17f92bf9869b0281ef7d3858e2d3f24c2d
Gabriel4224 <31612343+Gabriel4224@users.noreply.github.com>
2018-08-06 15:47:21 +1000

    Added -DrawerScript -OpenDrawerScript -Dresser prefab Updated -PlayerPrefab

---
+++ The House/Assets/Scripts/Interactable/DrawerScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawerScript : MonoBehaviour {
+
+    bool isOpen = false;
+    public float mouseY;
+    public float drawerSpeed; 
+    public GameObject Player;
+    public Rigidbody rb;
+    // Use this for initialization
+    void Start () {
+
+        isOpen = false;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        mouseY = Input.GetAxis("Mouse Y");
+
+        // lets go of the drawer when Mouse0 is released 
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+
+            isOpen = false;
+        }
+        // Doors position 
+        Vector3 drawerPosition = transform.position;
+        // Players position
+        Vector3 playerPosition = Player.transform.position;
+
+        // Gets the direction of the player from the door 
+        Vector3 Direction = drawerPosition - playerPosition;
+        Direction.Normalize();
+        if (isOpen)
+        {
+            Debug.Log("IsOpen");
+
+            rb.AddForceAtPosition(Player.transform.forward * mouseY * drawerSpeed, Player.transform.position);
+        }
+    }
+
+    public void changeDrawerState()
+    {
+        Debug.Log("ChangeDrawerState");
+        isOpen = !isOpen;
+    }
+}
--- The House/Assets/Scripts/Interactable/OpenDrawerScript.cs
+++ The House/Assets/Scripts/Interactable/OpenDrawerScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OpenDoorScript : MonoBehaviour {
+public class OpenDrawerScript : MonoBehaviour {
 
     // Distance in which the player can interact with the door 
     public float interactDistance;
@@ -11,22 +11,23 @@ public class OpenDoorScript : MonoBehaviour {
 
     public MonoBehaviour camScript;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         fpsCamera = GetComponent<FPSCamera>();
-
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+    // Update is called once per frame
+    void Update()
+    {
+
         //--------------------------------------------------------------------------------------
         // Raycasts in front of the player checking if their is a Door infront of the player 
         //
         // Param 
         //      Direction: the direction in which i want to check if there is a door 
         // Return 
-        //      Changes the doorState so that the door can be opened
+        //      Changes the DrawerState so that the door can be opened
         //--------------------------------------------------------------------------------------
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
@@ -34,16 +35,16 @@ public class OpenDoorScript : MonoBehaviour {
             Ray ray = new Ray(transform.position, transform.forward);
             RaycastHit hit;
             // checks if the player is in distance to open the door 
-            if (Physics.Raycast(ray,out hit,interactDistance))
+            if (Physics.Raycast(ray, out hit, interactDistance))
             {
 
-                if (hit.collider.CompareTag("Door"))
+                if (hit.collider.CompareTag("Drawer"))
                 {
                     //Goes into HingeDoorScript and allows player to open doors 
-                    hit.collider.transform.GetComponent<HingeDoorScript>().changeDoorState();
+                    hit.collider.transform.GetComponent<DrawerScript>().changeDrawerState();
 
                     //camScript.enabled = false;
-					fpsCamera.SetTouching(true);
+                    fpsCamera.SetTouching(true);
                 }
 
             }
@@ -59,9 +60,8 @@ public class OpenDoorScript : MonoBehaviour {
         if (Input.GetKeyUp(KeyCode.Mouse0))
         {
             //camScript.enabled = true;
-			fpsCamera.SetTouching(false);
+            fpsCamera.SetTouching(false);
         }
 
     }
 }
-
