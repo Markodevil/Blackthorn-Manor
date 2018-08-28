2c2f2c342809e0e3a1f9b761c82b17c13ec1eb11
Deeon Burgan <dburgan@live.com>
2018-08-28 10:35:22 +1000

    Updated ghost prefab Added Script listener for animation events

---
+++ The House/Assets/Scripts/AI/ScriptListener.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptListener : MonoBehaviour {
+
+    MenuManager mm;
+
+
+    private void Awake()
+    {
+        mm = GameObject.FindObjectOfType<MenuManager>();
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void LoadGameOver()
+    {
+        if (mm == null)
+            return;
+        mm.sceneName = "GameOver";
+        mm.fade.ResetTrigger("FadeIn");
+        mm.fade.SetTrigger("FadeOut");
+    }
+}
