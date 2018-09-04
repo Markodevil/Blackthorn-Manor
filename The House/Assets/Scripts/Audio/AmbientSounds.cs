58565f7639b8a2b25e2bf8eb12d8ae28559865da
Gabriel4224 <31612343+Gabriel4224@users.noreply.github.com>
2018-09-04 11:39:32 +1000

    Added AmbientSounds script which plays random sounds every 3 - 20 seconds Updated Player prefab to hold new Script and ambientsound source

---
+++ The House/Assets/Scripts/Audio/AmbientSounds.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbientSounds : MonoBehaviour {
+    public AudioSource audioSource;
+    public AudioClip[] Sounds;
+    private AudioClip playSound;
+    int soundIndex;
+    public float SoundTimer;
+    private int SoundTimerReset = 20;
+    bool RandomiseTimer = true;
+    bool PlaySoundEffect = false;
+    // Update is called once per frame
+    void Update () {
+	
+        if (RandomiseTimer)
+        {
+            SoundTimer = Random.Range(3, SoundTimerReset);
+            RandomiseTimer = false;
+        }
+        SoundTimer -= Time.deltaTime;
+
+        if (SoundTimer <= 0)
+        {
+            soundIndex = Random.Range(0, Sounds.Length);
+            playSound = Sounds[soundIndex];
+            audioSource.clip = playSound;
+            PlaySoundEffect = true;
+            RandomiseTimer = true;
+        }
+
+        if (PlaySoundEffect)
+        {
+            audioSource.Play();
+            PlaySoundEffect = false;
+        }
+    }
+}
