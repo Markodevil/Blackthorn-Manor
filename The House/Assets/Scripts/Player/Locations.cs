4d1d6e5c5d36caf18581f100ff005c02fff10b1c
Fryzeec <32441156+Fryzeec@users.noreply.github.com>
2018-09-12 12:13:01 +1000

    Added The LocationMarker.prefab and Locations.cs.

---
+++ The House/Assets/Scripts/Player/Locations.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Locations : MonoBehaviour
+{
+
+    public Text locationsText;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Get name and assign name to location UI
+        if (other.tag == "Player")
+        {
+            locationsText.text = gameObject.name;
+        }
+    }
+}
