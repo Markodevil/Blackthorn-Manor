9350dcf93ebf7cb143d17dadb2bb4248a008ac74
Deeon Burgan <dburgan@live.com>
2018-10-16 12:28:59 +1100

    Text writer added

---
+++ The House/Assets/Scripts/Managers/FancyTextWriter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FancyTextWriter : MonoBehaviour
+{
+
+    public Text UIText;
+    [Multiline]
+    public string desiredString;
+    [Multiline]
+    string currentString;
+    public float timeBetweenCharacters;
+    float timer;
+    int index = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentString = desiredString.Substring(0, index);
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            index++;
+            if (index <= desiredString.Length)
+            {
+                currentString = desiredString.Substring(0, index);
+                UIText.text = currentString;
+                timer = timeBetweenCharacters;
+
+            }
+        }
+    }
+}
